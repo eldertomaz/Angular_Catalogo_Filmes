fd87b9d8727b845320dfd0dc4fbd26f5fc58c78a
elder.tomazinho <elder.tomazinho@levty.com>
2021-03-18 10:08:08 -0300

    Validar error

--- curso-angular/src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ curso-angular/src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { ValidarCamposService } from 'src/app/shared/components/campos/validar-campos.service';
 
 @Component({
   selector: 'dio-cadastro-filmes',
@@ -10,7 +11,12 @@ export class CadastroFilmesComponent implements OnInit {
 
   cadastro: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(public validacao: ValidarCamposService,
+    private fb: FormBuilder) { }
+
+  get f() {
+    return this.cadastro.controls;
+  }
 
   ngOnInit() {
 
@@ -27,6 +33,7 @@ export class CadastroFilmesComponent implements OnInit {
   }
 
   salvar(): void{
+    this.cadastro.markAllAsTouched();
     if(this.cadastro.invalid){
       return;
     }
